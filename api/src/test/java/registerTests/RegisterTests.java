c21930f9335f3a9502a886d0fa131b9da01fcc6e
yana_mac <yana.mac89@gmail.com>
2021-10-31 01:38:55 +0200

    add first api test for user registration

--- api/src/test/java/registerTests/RegisterTests.java
+++ api/src/test/java/registerTests/RegisterTests.java
@@ -1,31 +1,71 @@
 package registerTests;
 
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.builder.ResponseSpecBuilder;
-import io.restassured.filter.log.LogDetail;
+import io.restassured.RestAssured;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeAll;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
-import static io.restassured.RestAssured.given;
+import payload.UserPayload;
 
 public class RegisterTests {
 
+    @BeforeClass
+    static void setUp(){
+        RestAssured.baseURI = "http://localhost:80";
+
+    }
+
     @Test
-    public void registerNewUser(){
-        given().spec(new RequestSpecBuilder()
-                    .log(LogDetail.ALL)
-                    .build())
-                .body("{\"username\":\"test\"," +
-                        "\"password\":\"12345\"," +
-                        "\"email\":\"_test\"," +
-                        "\"firstName\":\"test\"," +
-                        "\"lastName\":\"test\"}")
-        .when()
-                .post("http://localhost/register")
-        .then().spec(new ResponseSpecBuilder()
-                .log(LogDetail.ALL).build())
-                .assertThat().statusCode(200);
+    public void testCanRegisterNewUserWithValidCredentials() {
+        //given
+        UserPayload userPayload = new UserPayload()
+                .username(RandomStringUtils.randomAlphabetic(6))
+                .password("12345")
+                .email("demo@email.com");
 
+        //expect
+        registerNewUser(userPayload)
+                .then()
+                .body("id", Matchers.not(Matchers.isEmptyString()));
 
+    }
 
+    @Test
+    public void testCanNotCreateSameUserTwice(){
+        //given
+        UserPayload userPayload = new UserPayload()
+                .username(RandomStringUtils.randomAlphabetic(6))
+                .password("12345")
+                .email("demo@email.com");
+
+        //when
+        registerNewUser(userPayload)
+                .then()
+                .body("id", Matchers.not(Matchers.isEmptyString()));
+
+        //then
+        registerNewUser(userPayload)
+                .then().statusCode(500);
+
+    }
+
+    private RequestSpecification setup(){
+        return RestAssured
+                .given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
+    }
+    private Response registerNewUser(UserPayload userPayload){
+        return setup()
+                .body(userPayload)
+                .when()
+                .post("/register");
     }
 }
